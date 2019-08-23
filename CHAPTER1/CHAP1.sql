fc7bbaad1b19d3eb398396435a3de35de7c5af09
ndugu ndugu <ndugu.ndugu@ndugus-MacBook-Pro.local>
2019-08-23 10:07:05 +0200

    first commit

---
+++ CHAPTER1/CHAP1.sql
@@ -0,0 +1,7 @@
+#pg 17
+CREATE DATABASE gregs_list; 
+
+#--------------------------------
+
+#PG18
+USE gregs_list; 
---
+++ ch8/boys.sql
@@ -0,0 +1,8 @@
+insert into `boys`
+values 
+(1,'Davey',3),
+(2,'Bobby',5),
+(3,'Beaver',2),
+(4,'Richie',1),
+(6,'Johnny',4),
+(5,'Billy',2);
